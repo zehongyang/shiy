3f0e5bffedd8dff661330622a807ce913d8b7dd9
yangzehong <yangzehong@zolbon.com>
2019-12-02 15:12:03 +0800

    lang

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=127.0.0.1;dbname=shiy',
             'username' => 'root',
-            'password' => 'qwer!@#$',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
